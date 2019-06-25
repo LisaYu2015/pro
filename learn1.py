2286a624173d134707ff38f875cc11ed38f9d7ea
Lisa <lisayu1103@gmail.com>
2019-06-25 15:52:33 -0400

    replicate

--- learn1.py
+++ learn1.py
@@ -1,18 +1,11 @@
-# find duplicates in a list
-new_str = "my first day"
-new_ls =  list(new_str)
-print(new_ls)
 
-def dupl_search(list):
-    sorted_list = list.sort()
-    len_list = len(sorted_list)
+def dupl_search(input_list):
+    len_list = len(input_list)
+    input_list.sort()
+    dupl = []
     for i in range(0,(len_list - 1)):
-        if sorted_list(i) == sorted_list(i+1):
-            dup_item = sorted_list(i)
-    return sorted_list(i)
-
-
-duplicatte = dupl_search(new_ls)
-
-print(duplicatte)
-
+        if input_list[i] == input_list[i+1]:
+            dupl.append(input_list[i])
+    return dupl
+            
+print(dupl_search(new_ls))
