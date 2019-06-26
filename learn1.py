343661f29263ab914d7af723be7bb38b84f15d27
Lisa <lisayu1103@gmail.com>
2019-06-26 14:42:41 -0400

    count the replicates

--- learn1.py
+++ learn1.py
@@ -1,11 +1,23 @@
+new_str = "hello, this is Lisa"
+new_ls = list(new_str)
+print(new_ls)
 
-def dupl_search(input_list):
-    len_list = len(input_list)
-    input_list.sort()
-    dupl = []
-    for i in range(0,(len_list - 1)):
-        if input_list[i] == input_list[i+1]:
-            dupl.append(input_list[i])
-    return dupl
-            
-print(dupl_search(new_ls))
+def repli_count(input_list):
+    repl_dic = dict()
+    for i in input_list:
+        if i in repl_dic:
+            repl_dic[i] = repl_dic[i] + 1
+        else:
+            repl_dic[i] = 1
+    return repl_dic
+
+def eliminate_single(input_dic):
+    for i in input_dic:
+        if input_dic[i] == 1:
+            del input_dic[i]
+    return input_dic
+
+new_dic = repli_count(new_ls)
+print(new_dic)
+replic_dic = eliminate_single(new_dic)
+print(replic_dic)
